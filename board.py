1959e9f5ffc6c2d66deb5cf7e0171f2e3dc4001c
manvirchakal <mchakal31@gmail.com>
2022-03-14 20:18:18 -0500

    finally got movement working

--- board.py
+++ board.py
@@ -1,11 +1,12 @@
 class Square():
-	def __init__(self,index,name):
+	def __init__(self,index,name,coord):
 		self.index = index 
 		self.name = name 
+		self.coord = coord
 
 
 class Property(Square):
-	def __init__(self,index,name,price,rent,color,num_colors,is_mortgaged=False):
+	def __init__(self,index,name,price,rent,color,num_colors,coord,is_mortgaged=False):
 		self.index = index 
 		self.name = name 
 		self.price = price
@@ -15,6 +16,7 @@ class Property(Square):
 		self.is_owned = False
 		self.owner = None
 		self.num_colors = num_colors
+		self.coord = coord
 
 	def mortgage(self, player):
 		if self.is_mortgaged == False:
@@ -54,7 +56,7 @@ class Property(Square):
 
 
 class Street(Property):
-	def __init__(self,index,name,price,rent,color,rent1,rent2,rent3,rent4,rent5,build_cost,num_colors,is_mortgaged=False):
+	def __init__(self,index,name,price,rent,color,rent1,rent2,rent3,rent4,rent5,build_cost,num_colors,coord,is_mortgaged=False):
 		self.index = index 
 		self.name = name 
 		self.price = price 
@@ -70,51 +72,52 @@ class Street(Property):
 		self.num_houses = 0
 		self.is_owned = False
 		self.num_colors = num_colors
+		self.coord = coord
 
 
 class Board():
 	def __init__(self):
-		GO = Square(0, 'GO'),
-		medave = Street(1,'Mediterranean Ave',60,2,'Brown',10,30,90,160,250,50,2)
-		commchest1 = Square(2, 'Comm Chest')
-		baltave = Street(3,'Baltic Ave',60,4,'Brown',20,60,180,320,450,50,2)
-		inctax = Square(4, 'Income Tax')
-		rail1 = Property(5,'Reading Railroad',200, 25,'Railroad',4)
-		orientave = Street(6,'Oriental Ave',100,6,'Blue',30,90,270,400,550,50,3)
-		chance1 = Square(7, 'Chance')
-		vermave = Street(8,'Vermont Ave',100,6,'Blue',30,90,270,400,550,50,3)
-		connave = Street(9,'Connecticut Ave',120,8,'Blue',40,100,300,450,600,50,3)
-		justv = Square(10,'Just Visiting')
-		jail = Square(10, 'Jail')
-		scharlep = Street(11,'St. Charles Place',140,10,'Pink',50,150,450,625,750,100,3)
-		electc = Property(12,'Electric Comp',150,0,'Utility',2)
-		statesave = Street(13,'States Avenue',140,10,'Pink',50,150,450,625,750,100,3)
-		virgave = Street(14,'Virginia Ave',160,12,'Pink',60,180,500,700,900,100,3)
-		rail2 = Property(15,'Pennsylvania Railroad',200,25,'Railroad',4)
-		sjamesp = Street(16,'St. James Place',180,14,'Orange',70,200,550,750,950,100,3)
-		commchest2 = Square(17, 'Comm Chest')
-		tennave = Street(18,'Tennessee Ave',180,14,'Orange',70,200,550,750,950,100,3)
-		nyave = Street(19,'New York Ave',200,16,'Orange',80,220,600,800,1000,100,3)
-		freepark = Square(20, 'Free Parking')
-		kentave = Street(21,'Kentucky Ave',220,18,'Red',90,250,700,875,1050,150,3)
-		chance2 = Square(22, 'Chance')
-		indave = Street(23,'Indiana Ave',220,18,'Red',90,250,700,875,1050,150,3)
-		illave = Street(24,'Illinois Ave',240,20,'Red',100,300,750,925,1100,150,3)
-		rail3 = Property(25,'B&O Railroad',200,25,'Railroad',4)
-		atlave = Street(26,'Atlantic Ave',260,22,'Yellow',110,330,800,975,1150,150,3)
-		ventave = Street(27,'Ventor Ave',260,22,'Yellow',110,330,800,975,1150,150,3)
-		water = Property(28,'Water Works',150,0,'Utility',2)
-		marvgard = Street(29,'Marvin Gardens',280,24,'Yellow',120,360,850,1025,1200,150,3)
-		gotojail = Square(30, 'Go To Jail')
-		pacave = Street(31,'Pacific Ave',300,26,'Green',130,390,900,1100,1275,200,3)
-		ncarolave = Street(32, 'North Carolina Ave',300,26,'Green',130,390,900,1100,1275,200,3)
-		commchest3 = Square(33,'Comm Chest')
-		pennave = Street(34,'Pennsylvania Ave',320,28,'Green',150,450,1000,1200,1400,200,3)
-		rail4 = Property(35,'Short Line',200,25,'Railroad',4)
-		chance3 = Square(36, 'Chance')
-		parkplace = Street(37,'Park Place',350,35,'Purple',175,500,1100,1300,1500,200,2)
-		luxtax = Square(38,'Luxury Tax')
-		boardwalk = Street(39,'Boardwalk',400,50,'Purple',200,600,1400,1700,2000,200,2)
+		GO = Square(0, 'GO', (450,450)),
+		medave = Street(1,'Mediterranean Ave',60,2,'Brown',10,30,90,160,250,50,2,(410,450))
+		commchest1 = Square(2, 'Comm Chest',(360,450))
+		baltave = Street(3,'Baltic Ave',60,4,'Brown',20,60,180,320,450,50,2,(320,450))
+		inctax = Square(4, 'Income Tax',(280,450))
+		rail1 = Property(5,'Reading Railroad',200, 25,'Railroad',4,(240,450))
+		orientave = Street(6,'Oriental Ave',100,6,'Blue',30,90,270,400,550,50,3,(200,450))
+		chance1 = Square(7, 'Chance',(160,450))
+		vermave = Street(8,'Vermont Ave',100,6,'Blue',30,90,270,400,550,50,3,(110,450))
+		connave = Street(9,'Connecticut Ave',120,8,'Blue',40,100,300,450,600,50,3,(70,450))
+		justv = Square(10,'Just Visiting',(50,430))
+		jail = Square(10, 'Jail',(20,470))
+		scharlep = Street(11,'St. Charles Place',140,10,'Pink',50,150,450,625,750,100,3,(30,410))
+		electc = Property(12,'Electric Comp',150,0,'Utility',2,(30,360))
+		statesave = Street(13,'States Avenue',140,10,'Pink',50,150,450,625,750,100,3,(30,320))
+		virgave = Street(14,'Virginia Ave',160,12,'Pink',60,180,500,700,900,100,3,(30,280))
+		rail2 = Property(15,'Pennsylvania Railroad',200,25,'Railroad',4,(30,240))
+		sjamesp = Street(16,'St. James Place',180,14,'Orange',70,200,550,750,950,100,3,(30,200))
+		commchest2 = Square(17, 'Comm Chest',(30,160))
+		tennave = Street(18,'Tennessee Ave',180,14,'Orange',70,200,550,750,950,100,3,(30,120))
+		nyave = Street(19,'New York Ave',200,16,'Orange',80,220,600,800,1000,100,3,(30,80))
+		freepark = Square(20, 'Free Parking',(30,40))
+		kentave = Street(21,'Kentucky Ave',220,18,'Red',90,250,700,875,1050,150,3,(70,40))
+		chance2 = Square(22, 'Chance',(110,40))
+		indave = Street(23,'Indiana Ave',220,18,'Red',90,250,700,875,1050,150,3,(160,40))
+		illave = Street(24,'Illinois Ave',240,20,'Red',100,300,750,925,1100,150,3,(200,40))
+		rail3 = Property(25,'B&O Railroad',200,25,'Railroad',4,(240,40))
+		atlave = Street(26,'Atlantic Ave',260,22,'Yellow',110,330,800,975,1150,150,3,(280,40))
+		ventave = Street(27,'Ventor Ave',260,22,'Yellow',110,330,800,975,1150,150,3,(320,40))
+		water = Property(28,'Water Works',150,0,'Utility',2,(360,40))
+		marvgard = Street(29,'Marvin Gardens',280,24,'Yellow',120,360,850,1025,1200,150,3,(410,40))
+		gotojail = Square(30, 'Go To Jail',(450,40))
+		pacave = Street(31,'Pacific Ave',300,26,'Green',130,390,900,1100,1275,200,3,(450,80))
+		ncarolave = Street(32, 'North Carolina Ave',300,26,'Green',130,390,900,1100,1275,200,3,(450,120))
+		commchest3 = Square(33,'Comm Chest',(450,160))
+		pennave = Street(34,'Pennsylvania Ave',320,28,'Green',150,450,1000,1200,1400,200,3,(450,200))
+		rail4 = Property(35,'Short Line',200,25,'Railroad',4,(450,240))
+		chance3 = Square(36, 'Chance',(450,280))
+		parkplace = Street(37,'Park Place',350,35,'Purple',175,500,1100,1300,1500,200,2,(450,320))
+		luxtax = Square(38,'Luxury Tax',(450,370))
+		boardwalk = Street(39,'Boardwalk',400,50,'Purple',200,600,1400,1700,2000,200,2,(450,410))
 
 		self.tiles = [GO,
 				medave,
@@ -157,6 +160,7 @@ class Board():
 				luxtax,
 				boardwalk]
 
+
 	def get_tile_name(self,index):
 		if index == 10:
 			return self.tiles[index][0].name
@@ -166,8 +170,15 @@ class Board():
 
 		return self.tiles[index].name
 
-	def get_num_tiles(self):
-		print(len(self.tiles))
 
 	def get_type_square(self, index):
 		return type(self.tiles[index])
+
+
+	def get_square(self,index):
+		if type(self.tiles[index]) is tuple:
+			return self.tiles[index][0]
+		if type(self.tiles[index]) is list:
+			return self.tiles[index][0]
+		else:
+			return self.tiles[index]
--- main.py
+++ main.py
@@ -1,6 +1,66 @@
 from board import Square, Property, Street, Board 
 from player import Player
 from dice import Dice 
+import pygame
+import os
+
+WIDTH, HEIGHT = 1000,500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Monopoly")
+
+FPS = 60
+WHITE = (255,255,255)
+BOARD_IMAGE = pygame.image.load(os.path.join('Assets','board.jpeg'))
+BOARD = pygame.transform.scale(BOARD_IMAGE, (500,500))
+
+
+def take_turn(player,dice,board):
+	roll, double = dice.roll()
+
+	player.move(roll)
+	index = player.position
+	print(f"roll was {roll} and {player.name} is now at {board.get_tile_name(index)}")
+
+	if board.get_tile_name(index) == 'Comm Chest':
+		return
+
+	if board.get_tile_name(index) == 'Chance':
+		return
+
+	if index == 0:
+		return
+
+	if index == 4:
+		player.take_cash(200)
+		return
+
+	if index == 38:
+		player.take_cash(100)
+		return
+
+	if index == 10:
+		return
+
+	if index == 20:
+		return
+
+	if board.get_tile_name(index) == 'Go To Jail':
+		return
+
+	if not board.tiles[index].is_owned:
+		board.tiles[index].buy_property(player)
+		return
+
+	if board.tiles[index].is_owned:
+		owner = board.tiles[index].owner
+		player.take_cash(board.tiles[player.get_position()].get_rent(owner,board))
+		owner.add_cash(board.tiles[player.get_position()].get_rent(owner,board))
+		return
+
+	if double:
+		print("\n\n\n\n\n\n\nDOUBLE!!!!!!!!!!\n\n\n\n\n\n\n")
+		take_turn(player, dice, board)
+
 
 def decide_first(player1,player2,dice):
 	roll1 = dice.roll()
@@ -21,7 +81,69 @@ def decide_first(player1,player2,dice):
 		return
 
 
+def draw_window(player1,player2,red,green,board):
+	WIN.fill(WHITE)
+	WIN.blit(BOARD, (0,0))
+	pygame.draw.rect(WIN,(255,0,0),red)
+	pygame.draw.rect(WIN,(0,255,0),green)
+	pygame.display.update()
 
+
+def main():
+	board = Board()
+	dice = Dice()
+
+	player1 = Player(1500, 'Manny')
+	player2 = Player(1500, 'Ananty')
+
+	first = True
+	
+	clock = pygame.time.Clock()
+	run = True  
+	while run:
+		clock.tick(FPS)
+		for event in pygame.event.get(): 
+			index1 = player1.get_position()
+			index2 = player2.get_position()
+			at_tile1 = board.get_square(index1) 
+			at_tile2 = board.get_square(index2)
+			at_tile1_coord_x = at_tile1.coord[0]
+			at_tile1_coord_y = at_tile1.coord[1]
+			at_tile2_coord_x = at_tile2.coord[0]
+			at_tile2_coord_y = at_tile2.coord[1]
+			#print(f"{at_tile1_coord_x} {at_tile1_coord_y},{at_tile2_coord_x} {at_tile2_coord_y}")
+			red = pygame.Rect(at_tile1_coord_x, at_tile1_coord_y,10,10)
+			green = pygame.Rect(at_tile2_coord_x,at_tile2_coord_y+10,10,10)
+
+			if event.type == pygame.QUIT: 
+				run = False 
+
+			if event.type == pygame.KEYDOWN  and first:
+				if event.key == pygame.K_r:
+					first = False 
+					decide_first(player1, player2, dice)
+
+			if event.type == pygame.KEYDOWN: 
+				if event.key == pygame.K_r:
+					if player1.turn_pos:
+						take_turn(player1, dice, board)
+						take_turn(player2, dice, board)
+					
+					if player2.turn_pos:
+						take_turn(player2, dice, board)
+						take_turn(player1, dice, board)
+
+			draw_window(player1,player2,red,green,board)	
+
+	pygame.quit()
+
+
+
+if __name__ == "__main__": 
+	main()
+
+
+'''
 def take_turn(player,dice,board):
 	roll, double = dice.roll()
 
@@ -118,6 +240,6 @@ while num_turns < 50:
 			while board.get_type_square(player.get_position()) == Square:
 				prompt = input("not valid. Try again: ")
 
-			if (board.get_type_square(player.get_position()) == Property or board.get_type_square(player.get_position()) == Street) and not board.tiles[player.get_position()].is_owned::
+			if (board.get_type_square(player.get_position()) == Property or board.get_type_square(player.get_position()) == Street) and not board.tiles[player.get_position()].is_owned:
 				board.tiles[player1.get_position()].buy_property(player1)
-
+'''
